38c7891ca8e447bc6bf30540f96e224214caf4c0
xschen <xs0040@gmail.com>
2018-02-08 12:02:14 +0800

    tensorflow in place of keras

--- pydl_text_encoders/library/text_utils.py
+++ pydl_text_encoders/library/text_utils.py
@@ -1,4 +1,3 @@
-import nltk
 from collections import Counter
 
 
@@ -8,7 +7,7 @@ def get_most_frequent_words(X, max_vocab_size=None):
 
     counter = Counter()
     for sentence in X:
-        tokens = [w.lower() for w in nltk.word_tokenize(sentence)]
+        tokens = [w.lower() for w in sentence.split(' ')]
         for token in tokens:
             counter[token] += 1
     return set([word for word, idx in counter.most_common(max_vocab_size)])
